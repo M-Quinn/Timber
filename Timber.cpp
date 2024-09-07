7f01cf98bda646c5185d03f948ec5e8525a39361
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2024-09-07 13:40:41 -0400

    Added code for bee

--- Timber.cpp
+++ Timber.cpp
@@ -6,6 +6,10 @@ const int SCREEN_WIDTH = 1920;
 const int SCREEN_HEIGHT = 1080;
 const String SCREEN_TITLE = "Timber!";
 const String PATH_BACKGROUND_IMAGE = "graphics/background.png";
+const String PATH_TREE_IMAGE = "graphics/tree.png";
+const String PATH_BEE_IMAGE = "graphics/bee.png";
+const Vector2<float> TREE_POSITION = { 810,0 };
+const Vector2<float> BEE_POSITION = { 0,800 };
 
 int main()
 {
@@ -14,7 +18,15 @@ int main()
 	RenderWindow window(vm, SCREEN_TITLE, Style::Fullscreen);
 
 	Texture textureBackground;
+	Texture textureTree;
+	Texture textureBee;
 	Sprite spriteBackground;
+	Sprite spriteTree;
+	Sprite spriteBee;
+
+	bool beeActive = false;
+
+	float beeSpeed = 0.0f;
 
 	if(!textureBackground.loadFromFile(PATH_BACKGROUND_IMAGE))
 	{
@@ -23,6 +35,20 @@ int main()
 	spriteBackground.setTexture(textureBackground);
 
 	spriteBackground.setPosition(0, 0);
+
+	if (!textureTree.loadFromFile(PATH_TREE_IMAGE))
+	{
+		std::cout << "Error: Could not find Tree texture";
+	}
+	spriteTree.setTexture(textureTree);
+	spriteTree.setPosition(TREE_POSITION);
+
+	if(!textureBee.loadFromFile(PATH_BEE_IMAGE))
+	{
+		std::cout << "Error: Could not find bee texture";
+	}
+	spriteBee.setTexture(textureBee);
+	spriteBee.setPosition(BEE_POSITION);
 
 	while(window.isOpen())
 	{
@@ -39,6 +65,7 @@ int main()
 
 		//Draw everything
 		window.draw(spriteBackground);
+		window.draw(spriteTree);
 
 		//Display
 		window.display();
