efeff6ee0cf4cc9c3e7b63a6542173c3f678cae5
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2024-09-07 12:56:07 -0400

    drew the background image to the screen

--- Timber.cpp
+++ Timber.cpp
@@ -1,11 +1,24 @@
 #include<SFML/Graphics.hpp>
 using namespace sf;
 
+const int SCREEN_WIDTH = 1920;
+const int SCREEN_HEIGHT = 1080;
+const String SCREEN_TITLE = "Timber!";
+const String PATH_BACKGROUND_IMAGE = "graphics/background.png";
+
 int main()
 {
-	VideoMode vm(1920, 1080);
+	//Create the window
+	VideoMode vm(SCREEN_WIDTH, SCREEN_HEIGHT);
+	RenderWindow window(vm, SCREEN_TITLE, Style::Fullscreen);
 
-	RenderWindow window(vm, "Timber!", Style::Fullscreen);
+	Texture textureBackground;
+	Sprite spriteBackground;
+
+	textureBackground.loadFromFile(PATH_BACKGROUND_IMAGE);
+	spriteBackground.setTexture(textureBackground);
+
+	spriteBackground.setPosition(0, 0);
 
 	while(window.isOpen())
 	{
@@ -14,6 +27,12 @@ int main()
 		{
 			window.close();
 		}
+
+		window.clear();
+
+		window.draw(spriteBackground);
+
+		window.display();
 	}
 	return 0;
 }
