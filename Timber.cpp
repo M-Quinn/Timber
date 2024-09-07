b89191d598609eea02357bac763f6dc36bec0ed1
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2024-09-07 13:58:51 -0400

    Added the code for clouds

--- Timber.cpp
+++ Timber.cpp
@@ -8,8 +8,12 @@ const String SCREEN_TITLE = "Timber!";
 const String PATH_BACKGROUND_IMAGE = "graphics/background.png";
 const String PATH_TREE_IMAGE = "graphics/tree.png";
 const String PATH_BEE_IMAGE = "graphics/bee.png";
+const String PATH_CLOUD_IMAGE = "graphics/cloud.png";
 const Vector2<float> TREE_POSITION = { 810,0 };
 const Vector2<float> BEE_POSITION = { 0,800 };
+const Vector2<float> CLOUD1_POSITION = { 0,0 };
+const Vector2<float> CLOUD2_POSITION = { 0,250 };
+const Vector2<float> CLOUD3_POSITION = { 0,500 };
 
 int main()
 {
@@ -20,20 +24,25 @@ int main()
 	Texture textureBackground;
 	Texture textureTree;
 	Texture textureBee;
+	Texture textureCloud;
+	Sprite spriteClouds[3];
 	Sprite spriteBackground;
 	Sprite spriteTree;
 	Sprite spriteBee;
 
 	bool beeActive = false;
+	bool cloud1Active, cloud2Active, cloud3Active;
+	cloud1Active = cloud2Active = cloud3Active = false;
 
 	float beeSpeed = 0.0f;
+	float cloud1Speed, cloud2Speed, cloud3Speed;
+	cloud1Speed = cloud2Speed = cloud3Speed = 0.0f;
 
 	if(!textureBackground.loadFromFile(PATH_BACKGROUND_IMAGE))
 	{
 		std::cout << "Error: Could not find Background texture";
 	}
 	spriteBackground.setTexture(textureBackground);
-
 	spriteBackground.setPosition(0, 0);
 
 	if (!textureTree.loadFromFile(PATH_TREE_IMAGE))
@@ -49,6 +58,17 @@ int main()
 	}
 	spriteBee.setTexture(textureBee);
 	spriteBee.setPosition(BEE_POSITION);
+
+	if(!textureCloud.loadFromFile(PATH_CLOUD_IMAGE))
+	{
+		std::cout << "Error: Could not find cloud texture";
+	}
+	spriteClouds[0].setTexture(textureCloud);
+	spriteClouds[0].setPosition(CLOUD1_POSITION);
+	spriteClouds[1].setTexture(textureCloud);
+	spriteClouds[1].setPosition(CLOUD2_POSITION);
+	spriteClouds[2].setTexture(textureCloud);
+	spriteClouds[2].setPosition(CLOUD3_POSITION);
 
 	while(window.isOpen())
 	{
@@ -66,6 +86,11 @@ int main()
 		//Draw everything
 		window.draw(spriteBackground);
 		window.draw(spriteTree);
+		window.draw(spriteBee);
+		for (auto sprite_cloud : spriteClouds)
+		{
+			window.draw(sprite_cloud);
+		}
 
 		//Display
 		window.display();
